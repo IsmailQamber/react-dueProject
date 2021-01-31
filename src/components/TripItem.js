76d201d0b789f7b86d9dc492c6a526a02fb774f1
IsmailQamber <ism-you-95@hotmail.com>
2021-01-31 14:43:23 +0300

    updated but not done

--- src/components/TripItem.js
+++ src/components/TripItem.js
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+import hikingTrips from "../hiking";
 const TripItem = (props) => {
   return (
     <div>
-      <h3>{props.hikingTrip.name}</h3>
+      <Link to={`/trips/${props.hikingTrip.slug}`}>
+        <h3>{props.hikingTrip.name}</h3>
+      </Link>
       <h3>{props.hikingTrip.length}</h3>
     </div>
   );
